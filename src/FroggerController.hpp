1d092e870549f69508a59a4d6abea9732a0bda9b
meryla <119539604+meryla@users.noreply.github.com>
2024-06-20 15:41:31 +0200

    Update FroggerController.hpp

--- src/FroggerController.hpp
+++ src/FroggerController.hpp
@@ -1,4 +1,18 @@
-#ifndef FROGGERCONTROLLER_HPP
-#define FROGGERCONTROLLER_HPP
+#ifndef FROGGER_CONTROLLER_HPP
+#define FROGGER_CONTROLLER_HPP
+
+#include <SFML/Graphics.hpp>
+#include "FroggerModel.hpp"
+#include "FroggerView.hpp"
+
+class FroggerController {
+public:
+    FroggerController(FroggerModel* model, FroggerView* view);
+    void handleEvent(const sf::Event& event);
+    void update();
+private:
+    FroggerModel* model;
+    FroggerView* view;
+};
 
 #endif
