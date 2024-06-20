b3d5b1d3f090eaf4b1fc5ca6f9d6d1ec97e77aad
meryla <119539604+meryla@users.noreply.github.com>
2024-06-20 15:41:13 +0200

    Update FroggerController.cpp

--- src/FroggerController.cpp
+++ src/FroggerController.cpp
@@ -1,1 +1,22 @@
 #include "FroggerController.hpp"
+
+FroggerController::FroggerController(FroggerModel* model, FroggerView* view)
+    : model(model), view(view) {}
+
+void FroggerController::handleEvent(const sf::Event& event) {
+    Frog& frog = model->getFrog();
+    if (event.type == sf::Event::KeyPressed) {
+        switch (event.key.code) {
+            case sf::Keyboard::W: frog.moveUp(); break;
+            case sf::Keyboard::S: frog.moveDown(); break;
+            case sf::Keyboard::A: frog.moveLeft(); break;
+            case sf::Keyboard::D: frog.moveRight(); break;
+            default: break;
+        }
+    }
+    
+}
+
+void FroggerController::update() {
+    model->update();
+}
