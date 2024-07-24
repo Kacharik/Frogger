cdc83e2c9e37794aba9f6dd2168f016bf575677f
Kacharik <karthik.achari@outlook.com>
2024-07-24 18:59:29 +0200

    Turtle class

---
+++ src/ModelClasses/Turtle.cpp
@@ -0,0 +1,4 @@
+#include "Turtle.hpp"
+
+Turtle::Turtle(int x, int y, int width, int height, float position, float speed)
+    : LaneObject(x, y, width, height, position, speed) {}
---
+++ src/ModelClasses/Turtle.hpp
@@ -0,0 +1,11 @@
+#ifndef TURTLE_HPP
+#define TURTLE_HPP
+
+#include "LaneObject.hpp"
+
+class Turtle : public LaneObject {
+public:
+    Turtle(int x, int y, int width, int height, float position, float speed);
+};
+
+#endif // TURTLE_HPP
