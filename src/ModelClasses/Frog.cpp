0c0685aab1013219bcc6d8cf9e3a64894f1b5c38
meryla <119539604+meryla@users.noreply.github.com>
2024-07-25 22:36:42 +0200

    Update Frog.cpp

--- src/ModelClasses/Frog.cpp
+++ src/ModelClasses/Frog.cpp
@@ -1,17 +1,30 @@
 #include "Frog.hpp"
-
+#include <iostream>
 Frog::Frog(int x, int y, int width, int height)
     : Rectangle(x, y, width, height) {}
 
 void Frog::moveUp(){
-    setPosition(getX(), getY() - 1);
-};
+    std::cout << "Moving Up" << std::endl;
+    std::cout << "Current Position: (" << getX() << ", " << getY() << ")" << std::endl;
+    setPosition(getX(), getY() - 40);
+    std::cout << "New Position: (" << getX() << ", " << getY() << ")" << std::endl;
+}
+;
 void Frog::moveDown(){
-    setPosition(getX(), getY() + 1);
+    std::cout << "Moving down" << std::endl;
+    std::cout << "Current Position: (" << getX() << ", " << getY() << ")" << std::endl;
+    setPosition(getX(), getY() + 40);
+    std::cout << "New Position: (" << getX() << ", " << getY() << ")" << std::endl;
 };
 void Frog::moveLeft(){
-    setPosition(getX() - 1, getY());
+        std::cout << "Moving left" << std::endl;
+    std::cout << "Current Position: (" << getX() << ", " << getY() << ")" << std::endl;
+    setPosition(getX()-40, getY());
+    std::cout << "New Position: (" << getX() << ", " << getY() << ")" << std::endl;
 };
 void Frog::moveRight(){
-    setPosition(getX() + 1, getY());
+        std::cout << "Moving right" << std::endl;
+    std::cout << "Current Position: (" << getX() << ", " << getY() << ")" << std::endl;
+    setPosition(getX()+40, getY());
+    std::cout << "New Position: (" << getX() << ", " << getY() << ")" << std::endl;
 };
