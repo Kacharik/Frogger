7ade7c1b8c7f1ec1ec70db3afea510dd8deecbc9
meryla <119539604+meryla@users.noreply.github.com>
2024-08-08 16:39:42 +0200

    Update Log.hpp

--- src/ModelClasses/Log.hpp
+++ src/ModelClasses/Log.hpp
@@ -8,6 +8,7 @@ class Log : public LaneObject {
 public:
     Log(float x, float y, float width, float height, float speed);
     void update() override;
+const sf::RectangleShape& getShape() const override;
     const sf::Drawable& getDrawable() const override;
 };
 
