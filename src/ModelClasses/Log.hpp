1b20cc1e77e0ddbc55c4839fd23e2cfa0a1d2668
meryla <119539604+meryla@users.noreply.github.com>
2024-08-08 16:29:10 +0200

    draw fixing

--- src/ModelClasses/Log.hpp
+++ src/ModelClasses/Log.hpp
@@ -8,6 +8,7 @@ class Log : public LaneObject {
 public:
     Log(float x, float y, float width, float height, float speed);
     void update() override;
+    const sf::Drawable& getDrawable() const override;
 };
 
 #endif // LOG_HPP
