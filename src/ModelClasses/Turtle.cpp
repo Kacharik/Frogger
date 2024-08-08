b72a7e02cc8eecb5f8e73a64761b14c2f3b58bcc
meryla <119539604+meryla@users.noreply.github.com>
2024-08-08 12:01:46 +0200

    cant Turtle.cpp for now

--- src/ModelClasses/Turtle.cpp
+++ src/ModelClasses/Turtle.cpp
@@ -1,4 +1,4 @@
-#include "Turtle.hpp"
+/*#include "Turtle.hpp"
 
 Turtle::Turtle(int x, int y, int width, int height, float position, float speed)
-    : LaneObject(x, y, width, height, position, speed) {}
+    : LaneObject(x, y, width, height, position, speed) {}*/
