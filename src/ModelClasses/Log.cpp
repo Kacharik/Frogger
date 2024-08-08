7c4e6bf7ae22160eb8fc540ebd1522d382cee56d
meryla <119539604+meryla@users.noreply.github.com>
2024-08-08 16:29:27 +0200

    Update Log.cpp

--- src/ModelClasses/Log.cpp
+++ src/ModelClasses/Log.cpp
@@ -8,9 +8,12 @@ Log::Log(float x, float y, float width, float height, float speed)
 
 void Log::update() {
     shape.move(speed, 0); // Move horizontally based on speed
-    if (speed > 0 && shape.getPosition().x > 1300) {
+    if (speed > 0 && shape.getPosition().x > 1450) {
         shape.setPosition(-shape.getSize().x, shape.getPosition().y);
     } else if (speed < 0 && shape.getPosition().x + shape.getSize().x < 0) {
         shape.setPosition(1300, shape.getPosition().y);
     }
+}
+const sf::Drawable& Log::getDrawable() const {
+    return shape;
 }
