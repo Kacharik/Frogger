a1367e673842ff70acec34edf7c4a196498d71e8
meryla <119539604+meryla@users.noreply.github.com>
2024-08-08 14:43:13 +0200

    Update Car.hpp

--- src/ModelClasses/Car.hpp
+++ src/ModelClasses/Car.hpp
@@ -8,6 +8,13 @@ class Car : public LaneObject {
 public:
     Car(float x, float y, float width, float height, float speed);
     void update() override;
-};
+    const sf::Drawable& getDrawable() const override;
+   
+    void render(sf::RenderWindow& window) const; // Custom render function
 
+private:
+    sf::RectangleShape body; // Main body of the car
+    sf::CircleShape frontWheel; // Front wheel
+    sf::CircleShape rearWheel; // Rear wheel
+};
 #endif // CAR_HPP
