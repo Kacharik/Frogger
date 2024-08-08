3f8748dd866a42836b697226523d4dbba416c19d
meryla <119539604+meryla@users.noreply.github.com>
2024-08-08 12:00:22 +0200

    Update Log.cpp

--- src/ModelClasses/Log.cpp
+++ src/ModelClasses/Log.cpp
@@ -1,4 +1,16 @@
+// Log.cpp
 #include "Log.hpp"
 
-Log::Log(int x, int y, int width, int height, float position, float speed)
-    : LaneObject(x, y, width, height, position, speed) {}
+Log::Log(float x, float y, float width, float height, float speed)
+    : LaneObject(x, y, width, height, speed) {
+    shape.setFillColor(sf::Color(139, 69, 19)); // Example color (brown)
+}
+
+void Log::update() {
+    shape.move(speed, 0); // Move horizontally based on speed
+    if (speed > 0 && shape.getPosition().x > 1300) {
+        shape.setPosition(-shape.getSize().x, shape.getPosition().y);
+    } else if (speed < 0 && shape.getPosition().x + shape.getSize().x < 0) {
+        shape.setPosition(1300, shape.getPosition().y);
+    }
+}
