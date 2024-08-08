40eef64bb18adb979a1fea6a6544859bb395bf01
meryla <119539604+meryla@users.noreply.github.com>
2024-08-08 12:00:59 +0200

    comment Truck.cpp for now

--- src/ModelClasses/Truck.cpp
+++ src/ModelClasses/Truck.cpp
@@ -1,4 +1,4 @@
-#include "Truck.hpp"
+/*#include "Truck.hpp"
 
 Truck::Truck(int x, int y, int width, int height, float position, float speed)
-    : LaneObject(x, y, width, height, position, speed) {}
+    : LaneObject(x, y, width, height, position, speed) {}*/
