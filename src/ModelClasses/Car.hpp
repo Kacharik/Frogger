909addc348e8c8a0bfbfa4d9afefc9a2fc722ac8
meryla <119539604+meryla@users.noreply.github.com>
2024-08-09 11:31:01 +0200

    added virtual method Car.hpp

--- src/ModelClasses/Car.hpp
+++ src/ModelClasses/Car.hpp
@@ -8,8 +8,9 @@ class Car : public LaneObject {
 public:
     Car(float x, float y, float width, float height, float speed);
     void update() override;
-    const sf::Drawable& getDrawable() const override;
-   
+    const sf::RectangleShape& getShape() const override;
+    const sf::Drawable& getDrawable() const override; // Pure virtual function to get drawable
+
     void render(sf::RenderWindow& window) const; // Custom render function
 
 private:
