a16b6739e7de47bf163d9cd00d9eac3f5d64915a
meryla <119539604+meryla@users.noreply.github.com>
2024-08-17 14:58:42 +0200

    update for collisions

--- src/ModelClasses/Frog.hpp
+++ src/ModelClasses/Frog.hpp
@@ -4,14 +4,16 @@
 #include "Rectangle.hpp"
 #include <SFML/Graphics.hpp>
 
-class Frog : private Rectangle {
+class Frog : protected Rectangle {
+
 private:
     int windowWidth;
     int windowHeight;
+    sf::RectangleShape body;
 
 public:
     Frog(int x, int y, int width, int height, int windowWidth, int windowHeight);
-
+    
     int getCurrentX() const;
     int getCurrentY() const;
 
@@ -20,11 +22,14 @@ public:
     void moveLeft();
     void moveRight();
 
+    void move(float offsetX, float offsetY);
+    const sf::RectangleShape& getShape() const ;
     using Rectangle::getX;
     using Rectangle::getY;
     using Rectangle::getWidth;
     using Rectangle::getHeight;
     using Rectangle::setPosition;
+
     sf::FloatRect getFrogBounds() const;
     bool inBounds(int x, int y);
 
