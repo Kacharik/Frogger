6d47a467837556cb05fee40ca8d4b6d534b33b8c
meryla <119539604+meryla@users.noreply.github.com>
2024-08-18 20:16:49 +0200

    commented

--- src/Lanes/LaneObject.cpp
+++ src/Lanes/LaneObject.cpp
@@ -1,25 +1,25 @@
 #include "LaneObject.hpp"
-
+//lanes objects have (posistion)x,y and (size)width , height and the speed 
 LaneObject::LaneObject(float x, float y, float width, float height, float speed)
     : speed(speed) {
     shape.setPosition(x, y);
     shape.setSize(sf::Vector2f(width, height));
 }
-//position
+/****************************************POSITION****************************************/
 void LaneObject::setPosition(float x, float y) {
     shape.setPosition(x, y);
 }
 sf::Vector2f LaneObject::getPosition() const {
     return shape.getPosition();
 }
-//size
+/****************************************SIZE****************************************/
 void LaneObject::setSize(float width, float height) {
     shape.setSize(sf::Vector2f(width, height));
 }
 sf::Vector2f LaneObject::getSize() const {
     return shape.getSize();
 }
-//speed
+/****************************************SPEED****************************************/
 void LaneObject::setSpeed(float speed) {
     this->speed = speed;
 }
