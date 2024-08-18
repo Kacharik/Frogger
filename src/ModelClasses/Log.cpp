dbd3f496c72f03206387b0fcbd31ac8f04c11a15
meryla <119539604+meryla@users.noreply.github.com>
2024-08-18 20:22:56 +0200

    commented

--- src/ModelClasses/Log.cpp
+++ src/ModelClasses/Log.cpp
@@ -1,19 +1,20 @@
 // Log.cpp
 #include "Log.hpp"
-
+//same like cars and turtles...
 Log::Log(float x, float y, float width, float height, float speed)
     : LaneObject(x, y, width, height, speed) {
-    shape.setFillColor(sf::Color(139, 69, 19)); // Example color (brown)
+    shape.setFillColor(sf::Color(139, 69, 19)); 
 }
-
+/***********************************UPDATE*******************************************/
 void Log::update() {
-    shape.move(speed, 0); // Move horizontally based on speed
+    shape.move(speed, 0); //move horizontally based on speed
     if (speed > 0 && shape.getPosition().x > 1450) {
         shape.setPosition(-shape.getSize().x, shape.getPosition().y);
     } else if (speed < 0 && shape.getPosition().x + shape.getSize().x < 0) {
         shape.setPosition(1300, shape.getPosition().y);
     }
 }
+/***********************************GETTERS*******************************************/
 const sf::RectangleShape& Log::getShape() const {
     return shape;
 }
